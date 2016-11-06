72d6b66ae831f085f7ddddb976910a5698796db4
gejun123456 <gejunxxx@163.com>
2016-11-06 15:36:19 +0800

    add with content.

---
+++ src/main/java/com/rest/domain/Content.java
@@ -0,0 +1,7 @@
+package com.rest.domain;
+
+/**
+ * Created by bruce.ge on 2016/11/6.
+ */
+public class Content {
+}
---
+++ src/main/java/com/rest/mapper/ContentMapper.java
@@ -0,0 +1,7 @@
+package com.rest.mapper;
+
+/**
+ * Created by bruce.ge on 2016/11/6.
+ */
+public class ContentMapper {
+}
