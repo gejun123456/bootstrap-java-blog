6b1e3e87ab69124160552daff5e7feac71bd561c
gejun123456 <gejunxxx@163.com>
2016-11-12 20:01:55 +0800

    fix with name

--- sh/serverbuildjarpublish.sh
+++ sh/serverbuildjarpublish.sh
@@ -53,7 +53,7 @@ function run(){
     rm -r $dstLogFile
     touch $dstLogFile
     nohup nice java -jar $deployfilename --server.port=$serverPort >> $dstLogFile 2>&1 &
-    echo "COMMAND:nohup nice java -jar $destFile --server.port=$serverPort > $dstLogFile 2>&1 &"
+    echo "COMMAND:nohup nice java -jar $deployfilename --server.port=$serverPort > $dstLogFile 2>&1 &"
     echo ""
 }
 
