c72c0722fd03e00d76a6bfcd053b71538b941366
gejun123456 <gejunxxx@163.com>
2016-11-16 05:01:57 +0800

    add test

---
+++ src/test/java/com/rest/utils/StringUtilsTest.java
@@ -0,0 +1,13 @@
+package com.rest.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by bruce.ge on 2016/11/16.
+ */
+public class StringUtilsTest {
+    @Test
+    public void test(){
+        System.out.println("nimei");
+    }
+}
