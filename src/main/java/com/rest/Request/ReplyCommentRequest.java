46eb717f24e9468cffa8791658a38e76c98ced26
gejun123456 <gejunxxx@163.com>
2016-11-18 22:24:46 +0800

    move to commentcontroller

---
+++ src/main/java/com/rest/Request/ReplyCommentRequest.java
@@ -0,0 +1,7 @@
+package com.rest.Request;
+
+/**
+ * Created by bruce.ge on 2016/11/18.
+ */
+public class ReplyCommentRequest {
+}
---
+++ src/main/java/com/rest/utils/MessageSourceUtils.java
@@ -0,0 +1,7 @@
+package com.rest.utils;
+
+/**
+ * Created by bruce.ge on 2016/11/18.
+ */
+public class MessageSourceUtils {
+}
