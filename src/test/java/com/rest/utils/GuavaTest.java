6d78b4825fd27109edff5ec78e82d6aa298e69bb
gejun123456 <gejunxxx@163.com>
2017-01-05 19:49:09 +0800

    using with used table.

---
+++ src/test/java/com/rest/utils/GuavaTest.java
@@ -0,0 +1,17 @@
+package com.rest.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by bruce.ge on 2017/1/4.
+ */
+public class GuavaTest {
+    @Test
+    public void test(){
+//        Preconditions
+//        CharMatcher
+//        Joiner
+//        FluentIterable
+//        BloomFilter   default not exist or may exist.
+    }
+}
