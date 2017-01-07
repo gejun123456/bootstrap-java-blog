0eccc2bf901fa450085f82cebd830416244a9527
gejun123456 <gejunxxx@163.com>
2017-01-08 05:23:33 +0800

    add with test.

--- src/test/java/com/rest/mapper/ExecutionTimeLogDaoTest.java
+++ src/test/java/com/rest/mapper/ExecutionTimeLogDaoTest.java
@@ -1,12 +1,10 @@
 package com.rest.mapper;
-
 import basic.AbstractTest;
 import com.rest.domain.ExecutionTimeLog;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.junit.Assert.*;
+import java.util.Date;
 
 /**
  * Created by bruce.ge on 2017/1/8.
@@ -19,6 +17,13 @@ public class ExecutionTimeLogDaoTest extends AbstractTest {
     @Test
     public void insert() throws Exception {
         ExecutionTimeLog executionTimeLog = new ExecutionTimeLog();
+        executionTimeLog.setClassName("com.rest.hehe");
+        executionTimeLog.setMethodName("getName");
+        executionTimeLog.setExecutionTime(12321l);
+        executionTimeLog.setArgsValue("{cool:a,ooo:b}");
+        executionTimeLog.setCreateTime(new Date());
+        int insert = executionTimeLogDao.insert(executionTimeLog);
+        System.out.println(insert);
     }
 
     @Test
