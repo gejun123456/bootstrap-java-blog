89fd054e12ac809c12cd0ff4aa426d57ced10b42
gejun123456 <gejunxxx@163.com>
2017-01-26 21:36:38 +0800

    注释掉工具类main方法

--- src/main/java/com/generator/ProdTemplateGenerator.java
+++ src/main/java/com/generator/ProdTemplateGenerator.java
@@ -1,21 +1,9 @@
 package com.generator;
 
-import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @Author bruce.ge
@@ -33,50 +21,50 @@ public class ProdTemplateGenerator {
 
     public static final String PRODTEMPLATEPATH = "prod-templates";
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        Path path
-                = Paths.get("src/main/resources/" + DEVTEMPLATEPATH);
-
-
-        Path parent = path.getParent();
-        Path prodPath = parent.resolve(PRODTEMPLATEPATH);
-
-        if (Files.exists(prodPath)) {
-            FileUtils.deleteDirectory(prodPath.toFile());
-        }
-
-        File file = path.toFile();
-        List<File> freemarkFiles = Lists.newArrayList();
-        addToFreeMarker(file, freemarkFiles);
-
-        System.out.println("the freemarker files size is:" + freemarkFiles.size());
-
-        for (File freemarkFile : freemarkFiles) {
-            System.out.println("scan with:" + freemarkFile.getName());
-            Path relativize = path.relativize(freemarkFile.toPath());
-            System.out.println("the relativize path is:" + relativize);
-            Path resolve = prodPath.resolve(relativize);
-            System.out.println("the resolved path is:" + resolve.toString());
-            String freemarkerString = IOUtils.toString(new FileInputStream(freemarkFile), Charset.defaultCharset());
-            System.out.println("read freemarker file:" + freemarkFile.getName() + " to String success");
-            String replaceString = freemarkerString;
-            for (ReplacePair replacePair : replacePairs) {
-                if (replaceString.contains(replacePair.getDevPath())) {
-                    System.out.println("find " + replacePair.getDevPath() + " from " + freemarkFile.getName() + " gonna replace it");
-                    replaceString = replaceString.replaceAll(replacePair.getDevPath(), replacePair.getCdnPath());
-                }
-            }
-            System.out.println("start write to file:" + resolve.toString());
-            File m = new File(resolve.toString());
-            m.getParentFile().mkdirs();
-            FileOutputStream output = new FileOutputStream(m);
-            IOUtils.write(replaceString, output, Charset.defaultCharset());
-            System.out.println("write to file:" + resolve.toString() + " success");
-        }
-        stopwatch.stop();
-        System.out.println("the time cost is:" + stopwatch.elapsed(TimeUnit.SECONDS));
-    }
+//    public static void main(String[] args) throws URISyntaxException, IOException {
+//        Stopwatch stopwatch = Stopwatch.createStarted();
+//        Path path
+//                = Paths.get("src/main/resources/" + DEVTEMPLATEPATH);
+//
+//
+//        Path parent = path.getParent();
+//        Path prodPath = parent.resolve(PRODTEMPLATEPATH);
+//
+//        if (Files.exists(prodPath)) {
+//            FileUtils.deleteDirectory(prodPath.toFile());
+//        }
+//
+//        File file = path.toFile();
+//        List<File> freemarkFiles = Lists.newArrayList();
+//        addToFreeMarker(file, freemarkFiles);
+//
+//        System.out.println("the freemarker files size is:" + freemarkFiles.size());
+//
+//        for (File freemarkFile : freemarkFiles) {
+//            System.out.println("scan with:" + freemarkFile.getName());
+//            Path relativize = path.relativize(freemarkFile.toPath());
+//            System.out.println("the relativize path is:" + relativize);
+//            Path resolve = prodPath.resolve(relativize);
+//            System.out.println("the resolved path is:" + resolve.toString());
+//            String freemarkerString = IOUtils.toString(new FileInputStream(freemarkFile), Charset.defaultCharset());
+//            System.out.println("read freemarker file:" + freemarkFile.getName() + " to String success");
+//            String replaceString = freemarkerString;
+//            for (ReplacePair replacePair : replacePairs) {
+//                if (replaceString.contains(replacePair.getDevPath())) {
+//                    System.out.println("find " + replacePair.getDevPath() + " from " + freemarkFile.getName() + " gonna replace it");
+//                    replaceString = replaceString.replaceAll(replacePair.getDevPath(), replacePair.getCdnPath());
+//                }
+//            }
+//            System.out.println("start write to file:" + resolve.toString());
+//            File m = new File(resolve.toString());
+//            m.getParentFile().mkdirs();
+//            FileOutputStream output = new FileOutputStream(m);
+//            IOUtils.write(replaceString, output, Charset.defaultCharset());
+//            System.out.println("write to file:" + resolve.toString() + " success");
+//        }
+//        stopwatch.stop();
+//        System.out.println("the time cost is:" + stopwatch.elapsed(TimeUnit.SECONDS));
+//    }
 
     private static void addToFreeMarker(File file, List<File> freemarkFiles) {
         File[] files = file.listFiles();
